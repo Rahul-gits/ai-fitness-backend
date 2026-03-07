aae3f2ce424a86ccf7a4240d87bb03722bb9ecc7
Rahul-gits <rahulgunda206@gmail.com>
2026-03-07 16:06:12 +0530

    Optimize dependencies for Railway deployment

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from ..core.config import settings
-
 # Async database setup
 ASYNC_DATABASE_URL = settings.get_database_url(is_async=True)
 async_engine = create_async_engine(
     ASYNC_DATABASE_URL,
-    echo=True,
+    echo=False,  # DISABLED - was causing excessive logging
     future=True,
     pool_pre_ping=True
 )
@@ -19,12 +14,12 @@ AsyncSessionLocal = async_sessionmaker(
     autocommit=False,
 )
 
-# Sync database setup (for Alembic or special sync tasks)
+# Sync database setup
 SYNC_DATABASE_URL = settings.get_database_url(is_async=False)
 if SYNC_DATABASE_URL.startswith("sqlite"):
-    sync_engine = create_engine(SYNC_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+    sync_engine = create_engine(SYNC_DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
 else:
-    sync_engine = create_engine(SYNC_DATABASE_URL, echo=True)
+    sync_engine = create_engine(SYNC_DATABASE_URL, echo=False)
 
 SessionLocal = sessionmaker(
     autocommit=False,
